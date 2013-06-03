05797ad71e0cd9dd404331e0b24f96d45f82286a
Florent <fteissier@teads.tv>
2013-06-03 17:22:01 +0200

    first commit

---
+++ jquery-github.js
@@ -0,0 +1,8 @@
+(function($)
+{
+	//https://api.github.com/repos/flocks/Cornelien/commits
+	var baseUrl  = 'https://api.github.com/repos/';
+	$.fn.githubCommits = function(user, repo, nb_commits) {
+		console.log(user);
+	}
+})(jQuery);
